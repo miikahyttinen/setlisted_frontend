7de044876b006fa29727600c9cbfd06a9b5ed4bc
Miika Hyttinen <hyttinenmiika@gmail.com>
2019-09-04 14:51:59 +0300

    spotify api auth

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 
 const App = () => {
-  const [songs, setSongs] = useState([])
+  const [params, setParams] = useState('')
+
+  console.log('REAL PARAMS', params)
+
 
   useEffect(() => {
-    console.log('effect happened')
-    axios.get('http://localhost:3001/songs').then(response => {
-      setSongs(response.data)
-    })
+    setParams(getHashParams())
   }, [])
+    
+
+  const getHashParams = () => {
+    var hashParams = {}
+    var e,
+      r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1)
+    while ((e = r.exec(q))) {
+      hashParams[e[1]] = decodeURIComponent(e[2])
+    }
+    return hashParams
+  }
 
   return (
+    <div className='App'>
+      <a href='http://localhost:8888'>
+        <button>Login with Spotify</button>
+      </a>
+    </div>
+  )
+}
+/*
     <div>
       <h1>Welcome to Setlisted</h1>
       {songs.map(song => {
         return <div> {song.name} </div>
       })}
-    </div>
-  )
-}
+    </div> */
 
 export default App
