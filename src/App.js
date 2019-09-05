d658cbc7f691cc24762b7b722d246255dc956d24
Miika Hyttinen <hyttinenmiika@gmail.com>
2019-09-05 19:16:09 +0300

    hours updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
 import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import queryString from 'query-string'
+import axios from 'axios'
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0.5em 1em;
+  padding: 0.25em 1em;
+`
+
+const Page = styled.div`
+  background: #daf7a6;
+  height: 100%;
+  text-align: center;
+`
 
 const App = () => {
-  const [params, setParams] = useState('')
-
-  console.log('REAL PARAMS', params)
-
+  const [accessToken, setAccessToken] = useState('')
 
   useEffect(() => {
-    setParams(getHashParams())
+    setAccessToken(queryString.parse(window.location.search).access_token)
+    axios
+      .get('http://localhost:3001/spotify')
+      .then(response => console.log('RES', response.data))
   }, [])
-    
-
-  const getHashParams = () => {
-    var hashParams = {}
-    var e,
-      r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1)
-    while ((e = r.exec(q))) {
-      hashParams[e[1]] = decodeURIComponent(e[2])
-    }
-    return hashParams
-  }
 
   return (
-    <div className='App'>
-      <a href='http://localhost:8888'>
-        <button>Login with Spotify</button>
+    <Page>
+      <a href='https://setlisted-oauth-service.herokuapp.com/login'>
+        <Button>Login with Spotify</Button>
       </a>
-    </div>
+    </Page>
   )
 }
 /*
